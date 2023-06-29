06be6e7a479430665a6ac6166e93a1d8657bbd39
Isaac Abarca Camacho <69941209+Isaac-Abarca@users.noreply.github.com>
2023-06-28 22:19:18 -0600

    cambios routeo

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,19 @@ import Navbar from "./components/Navbar";
 import Pricing from "./pages/Pricing";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
       <div className="container">
-        <BrowserRouter>
           <Routes>
             <Route exac path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
             <Route path="/pricing" element={<Pricing />} />
             <Route path="/about" element={<About />} />
           </Routes>
-        </BrowserRouter>
       </div>
     </>
   );
